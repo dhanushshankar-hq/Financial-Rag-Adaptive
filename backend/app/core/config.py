3a921a5c63560e167fcbd3ba8613fe8ed67ae63c
Coderdhanush2003 <dhanush2003.shankar@gmail.com>
2026-08-11 18:40:13 +0530

    feat: setup project skeleton and config management

---
+++ backend/app/core/config.py
@@ -0,0 +1,36 @@
+from pydantic_settings import BaseSettings,SettingsConfigDict
+from typing import Optional
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "finrag-adaptive"
+    API_V1_STR: str = "/api/v1"
+    
+    # API Keys
+    GROQ_API_KEY: str
+    GEMINI_API_KEY: str
+    TOGETHER_API_KEY: str
+    
+    # PostgreSQL Configuration
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str = "finrag"
+    
+    # Neo4j Configuration
+    NEO4J_URI: str = "neo4j://localhost:7687"
+    NEO4J_USER: str = "neo4j"
+    NEO4J_PASSWORD: str = "password"
+    
+    # Redis Configuration
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+
+    @property
+    def ASYNC_POSTGRES_URI(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+    print(model_config)
+    
+setting = Settings()
